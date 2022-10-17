3e576cf3491bebee15e2572cd2217d847b661ec0
yanooomm <115118650+yanooomm@users.noreply.github.com>
2022-10-17 11:45:52 +0500

    Add files via upload

---
+++ лаб4-1.cpp
@@ -0,0 +1,34 @@
+﻿/* TextFile5 Дан текстовый файл, в котором записано несколько
+натуральных чисел. Посчитать среднее арифметическое чисел, 
+хранимых в файле.*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    ifstream f;
+    f.open("TextFile5.txt");
+    float x;
+    int sum = 0, k = 0;
+    if (!f.is_open())
+    {
+        cout << "Ошибка открытия файла";
+    }
+    else
+    {
+        while (f >> x)
+        {
+            sum += x;
+            k += 1;
+        }
+        cout << sum / k;
+    }
+}
+
+//1 2 3 -> 2
+//123 456 789 -> 456
+//98 54 32 5 -> 47
